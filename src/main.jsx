92e10c6219a8de9e6da4ba81617d1f65e204e74a
Audrey <133895818+audreytho@users.noreply.github.com>
2026-01-28 20:26:31 -0500

    idek bro

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-
-// Initialize animations
-AOS.init({
-  duration: 800,
-  once: true,
-  easing: 'ease-out'
-})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
