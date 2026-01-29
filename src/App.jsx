6c8bc6171d8f0adb686ba684fe923b392a590523
Audrey <133895818+audreytho@users.noreply.github.com>
2026-01-29 12:55:49 -0500

    :P x2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
+import Home from './pages/Home'
 import Work from './pages/Work'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -8,17 +9,33 @@ import './App.css'
 function App() {
     return (
         <Router>
-            <div className="App">
-                <Header />
-                <main>
-                    <Routes>
-                        <Route path="/" element={<About />} />
-                        <Route path="/work" element={<Work />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/contact" element={<Contact />} />
-                    </Routes>
-                </main>
-            </div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/work" element={
+                    <div className="App">
+                        <Header />
+                        <main><Work /></main>
+                    </div>
+                } />
+                <Route path="/about" element={
+                    <div className="App">
+                        <Header />
+                        <main><About /></main>
+                    </div>
+                } />
+                <Route path="/contact" element={
+                    <div className="App">
+                        <Header />
+                        <main><Contact /></main>
+                    </div>
+                } />
+                <Route path="/resume" element={
+                    <div className="App">
+                        <Header />
+                        <main><Contact /></main>
+                    </div>
+                } />
+            </Routes>
         </Router>
     )
 }
